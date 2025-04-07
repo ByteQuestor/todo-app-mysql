7c44f6a56f52c47c1f0856a533228380396724ec
Mathieu Meylan <m.meylan@depsys.com>
2025-04-07 08:44:06 +0200

    add dotenv for better node 20+ compatibility

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
-process.loadEnvFile('./.env');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || '3000';
 const env = process.env.NODE_ENV || 'development';
